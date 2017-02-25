65af57698904a909ad267fbad17fa45eb89a3422
Alan Pierce <alangpierce@gmail.com>
2017-02-25 08:23:11 -0800

    Add a code prefix to enable babel for the atom project (#19)

--- examples/atom/bulk-decaffeinate.config.js
+++ examples/atom/bulk-decaffeinate.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   fileFilterFn: path => !path.includes('spec/fixtures'),
   decaffeinateArgs: ['--keep-commonjs', '--prefer-const', '--enable-babel-constructor-workaround'],
+  codePrefix: '/** @babel */\n',
 };
