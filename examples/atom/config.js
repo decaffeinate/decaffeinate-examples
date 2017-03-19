488851592e1eca6805c118a447d5363ba2534a85
Alan Pierce <alangpierce@gmail.com>
2017-03-19 15:57:32 -0700

    Install nvm in atom tests (#35)

--- examples/atom/config.js
+++ examples/atom/config.js
@@ -4,11 +4,14 @@ export default {
   useDefaultConfig: true,
   testCommand: `
     set -e
+    export NODE_VERSION=6.9.4 DISPLAY=:99.0 CC=clang CXX=clang++ npm_config_clang=1
     if [ -e /sbin/start-stop-daemon ]; then
       /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16
     fi
-    export NODE_VERSION=6.9.4 DISPLAY=:99.0 CC=clang CXX=clang++ npm_config_clang=1
-    nvm install 6.9.4
+    git clone https://github.com/creationix/nvm.git /tmp/.nvm
+    source /tmp/.nvm/nvm.sh
+    nvm install $NODE_VERSION
+    nvm use --delete-prefix $NODE_VERSION
     script/build --create-debian-package --create-rpm-package --compress-artifacts
     script/test
   `,
