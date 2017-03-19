90bfcb29249aa350d3ef09dffb47378f7bff58ae
Alan Pierce <alangpierce@gmail.com>
2017-03-19 15:03:00 -0700

    Add transform for top-level `this` to CoffeeScript

--- examples/coffeescript/bulk-decaffeinate.config.js
+++ examples/coffeescript/bulk-decaffeinate.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   fileFilterFn: path => !path.includes('test/importing/') && !path.includes('documentation'),
+  jscodeshiftScripts: [
+    'top-level-this-to-exports.js',
+  ],
   decaffeinateArgs: ['--keep-commonjs', '--prefer-const', '--enable-babel-constructor-workaround'],
 };
