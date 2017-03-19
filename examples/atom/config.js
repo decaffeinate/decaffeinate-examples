e3a698957d316b27ac10d0a0b827bcb488ec4d6a
Alan Pierce <alangpierce@gmail.com>
2017-03-18 22:55:11 -0700

    Change the atom test config to be a little closer to the normal build (#29)
    I think the tests should all pass now but the build is just misconfigured, so
    hopefully tweaking it to be closer to the atom travis.yml will fix things.

--- examples/atom/config.js
+++ examples/atom/config.js
@@ -2,7 +2,12 @@ export default {
   cloneUrl: 'https://github.com/atom/atom.git',
   forkUrl: 'git@github.com:decaffeinate-examples/atom.git',
   useDefaultConfig: true,
-  testCommand: 'rm -rf ./node_modules && script/build && script/test',
+  testCommand: `
+    set -e
+    script/clean
+    script/build --create-debian-package --create-rpm-package --compress-artifacts
+    script/test
+  `,
   expectConversionSuccess: true,
   expectTestSuccess: false,
 };
