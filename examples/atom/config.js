1df4f1299a7dab3351386e09cb06a89b705601fc
Alan Pierce <alangpierce@gmail.com>
2017-03-19 15:27:20 -0700

    Make atom tests closer to the atom travis file (#34)
    This will hopefully make the tests work on CI.

--- examples/atom/config.js
+++ examples/atom/config.js
@@ -4,6 +4,11 @@ export default {
   useDefaultConfig: true,
   testCommand: `
     set -e
+    if [ -e /sbin/start-stop-daemon ]; then
+      /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16
+    fi
+    export NODE_VERSION=6.9.4 DISPLAY=:99.0 CC=clang CXX=clang++ npm_config_clang=1
+    nvm install 6.9.4
     script/build --create-debian-package --create-rpm-package --compress-artifacts
     script/test
   `,
