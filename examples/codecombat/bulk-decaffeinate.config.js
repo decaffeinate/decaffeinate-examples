21961eab17991a81a3e310ec8a2726488cf9c2c8
Alan Pierce <alangpierce@gmail.com>
2017-03-25 18:42:49 -0700

    Remove .coffee from imports in codecombat

--- examples/codecombat/bulk-decaffeinate.config.js
+++ examples/codecombat/bulk-decaffeinate.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  jscodeshiftScripts: [
+    'remove-coffee-from-imports.js',
+  ],
   fileFilterFn: path => !path.includes('bower_components/'),
   decaffeinateArgs: ['--keep-commonjs', '--prefer-const', '--enable-babel-constructor-workaround'],
 };
