a45bf7ec497c60436e13b29d8de748883cebbe71
Alan Pierce <alangpierce@gmail.com>
2017-03-26 09:15:49 -0700

    Disable strict mode for codecombat
    Some test init code was accidentally using `this` in a function called without
    any `this` bound.

--- examples/codecombat/config.js
+++ examples/codecombat/config.js
@@ -2,6 +2,9 @@ export default {
   cloneUrl: 'https://github.com/codecombat/codecombat.git',
   forkUrl: 'git@github.com:decaffeinate-examples/codecombat.git',
   useDefaultConfig: true,
+  extraDependencies: [
+    'babel-plugin-transform-remove-strict-mode',
+  ],
   skipTests: true,
   expectConversionSuccess: true,
   expectTestSuccess: false,
