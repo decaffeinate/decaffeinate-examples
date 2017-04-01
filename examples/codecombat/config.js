a6dba1c6d5bec9f64b4cbbbfe6e06509ada6fbe2
Alan Pierce <alangpierce@gmail.com>
2017-04-01 13:37:01 -0700

    Set up codecombat build and tests (#45)
    There may be some issue here, but this should at least be a reasonable first
    pass.

--- examples/codecombat/config.js
+++ examples/codecombat/config.js
@@ -4,8 +4,27 @@ export default {
   useDefaultConfig: true,
   extraDependencies: [
     'babel-plugin-transform-remove-strict-mode',
+    'babel-brunch@6.0.0',
   ],
-  skipTests: true,
+  testCommand: `
+    set -e
+    export COCO_TRAVIS_TEST=1
+    export DISPLAY=:99.0
+    if [ -e /etc/init.d/xvfb ]; then
+      sh -e /etc/init.d/xvfb start
+    fi
+    rm -rf node_modules
+    rm -rf bower-components
+    rm -rf public
+    npm install
+    
+    node index.js --unittest &
+    n=0
+    until [ $n -ge 60 ]; do curl http://localhost:3000 && break; n=$[$n+1]; sleep 1; done
+    
+    ./node_modules/karma/bin/karma start --browsers Firefox --single-run --reporters dots
+    npm run jasmine
+  `,
   expectConversionSuccess: true,
   expectTestSuccess: false,
 };
