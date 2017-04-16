4a4ab638571e4b83c5a6b7826a6ab0eabe3f908d
Alan Pierce <alangpierce@gmail.com>
2017-04-16 13:44:03 -0700

    Add chroma.js as an example project (#52)
    This project uses a custom build system that just concatenates all files but
    also has a system for declaring dependencies through comments, so I had to
    change the converted files to have the new format and also change two `const`
    declarations to `var` since decaffeinate expects that each file is a
    self-contained scope.

---
+++ examples/chroma.js/config.js
@@ -0,0 +1,19 @@
+export default {
+  cloneUrl: 'https://github.com/gka/chroma.js.git',
+  forkUrl: 'git@github.com:decaffeinate-examples/chroma.js.git',
+  useDefaultConfig: true,
+  extraDependencies: [
+    'grunt-cli',
+  ],
+  expectConversionSuccess: true,
+  expectTestSuccess: true,
+  testCommand: `
+    set -e
+    find src -name '*.js' | xargs sed -i '' -e 's/\\/\\/ @require\\(.*\\)$/\\/* @require\\1 *\\//g'
+    git commit -a -m 'Change catty dependencies to have the proper format'
+    
+    npm run build
+    git commit -a -m 'Rebuild chroma.js'
+    npm test
+  `,
+};
