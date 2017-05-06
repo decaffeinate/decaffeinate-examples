507e92dc90732ce1b28e37632b6da4aeb0469846
Alan Pierce <alangpierce@gmail.com>
2017-05-06 12:16:43 -0700

    Pin autoprefixer version to 6.7.7 (#63)
    Version 7.0.0 is now JavaScript thanks to decaffeinate! So we'll just run the
    build on the last version before the switch to JS.

--- examples/autoprefixer/config.js
+++ examples/autoprefixer/config.js
@@ -1,6 +1,7 @@
 export default {
   cloneUrl: 'https://github.com/postcss/autoprefixer.git',
   forkUrl: 'git@github.com:decaffeinate-examples/autoprefixer.git',
+  branch: '6.7.7',
   useDefaultConfig: true,
   expectConversionSuccess: true,
   expectTestSuccess: true,
