1236ff59b1a874a09511742c2402494834918479
Alan Pierce <alangpierce@gmail.com>
2017-06-09 07:50:43 -0700

    Pin hubot to the commit just before switching to JS

--- examples/hubot/config.js
+++ examples/hubot/config.js
@@ -1,6 +1,7 @@
 export default {
   cloneUrl: 'https://github.com/github/hubot.git',
   forkUrl: 'git@github.com:decaffeinate-examples/hubot.git',
+  commit: '846aec66563baf61f1178ac8a6970f0e7a3df6e9',
   useDefaultConfig: true,
   expectConversionSuccess: true,
   expectTestSuccess: true,
